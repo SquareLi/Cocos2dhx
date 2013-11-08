29691f92d81cd249c332f41e36d5112b9137c7e4
unknown <ang.li139@gmail.com>
2013-11-08 14:40:33 -0500

    TileSample1 update

--- Sample/TiledMapSamples/TiledSample1/Readme.md
+++ Sample/TiledMapSamples/TiledSample1/Readme.md
@@ -1,7 +1,3 @@
 [Play it!](https://googledrive.com/host/0B8wpb3e7SGg4NkIxMW5ZemEzWGs/index.html)
 
-On computer, use keyboard to control the ninja.
-
-On mobile devices, there are four touch zones on screen. If you touch the left side of the screen, ninja will go left. 
-
-The game will only draw the part of map that is in the viewport. 
+The game only draws the part of map that is in the viewport. 
--- Sample/TiledMapSamples/TiledSample1/src/urgame/TestLayer.hx
+++ Sample/TiledMapSamples/TiledSample1/src/urgame/TestLayer.hx
@@ -11,6 +11,7 @@ import cc.cocoa.CCGeometry;
 import cc.support.CCPointExtension;
 import cc.CCDirector;
 import flambe.math.Rectangle;
+import cc.action.CCActionInterval;
 /**
  * ...
  * @author Ang Li(李昂)
@@ -24,6 +25,9 @@ class TestLayer extends CCLayer
 	var offset : Point;
 	var isMove : Bool = false;
 	var viewPort : Rectangle;
+	var playerVec : Float = 16;
+	var speedY : Float = 0;
+	var speedX : Float = 0;
 	public function new() 
 	{
 		super();
@@ -44,6 +48,7 @@ class TestLayer extends CCLayer
 		this.setKeyboardEnabled(true);
 		offset = new Point();
 		metaLayer = map.getLayer("Meta");
+		oldPosition = new Point(player.getPositionX(), player.getPositionY());
 		
 		
 		//Get the objects layer
@@ -54,15 +59,20 @@ class TestLayer extends CCLayer
 		
 		
 		viewPort = new Rectangle(0, 0, CCDirector.getInstance().getWinSize().width, CCDirector.getInstance().getWinSize().height);
-
-		
+		des = new Point();
+		viewPort.x = 0 - this.getPositionX();
+		viewPort.y = 0 - this.getPositionY();
+		//Update the viewport
+		CCTMXTiledMap.viewPort = viewPort;
 	}
 	
+	var oldPosition : Point;
 	override public function update(dt:Float)
 	{
 		super.update(dt);
 		if (isMove) {
-			var p = CCPointExtension.pAdd(player.getPosition(), offset);
+			var offset = new Point(speedX, speedY);
+			var p : Point = CCPointExtension.pAdd(offset, player.getPosition());
 			if (!checkCollision(p)) {
 				player.setPosition(p.x, p.y);
 				this.setPosition(this.getPositionX() - offset.x, this.getPositionY() - offset.y);
@@ -72,21 +82,19 @@ class TestLayer extends CCLayer
 				viewPort.x = 0 - this.getPositionX();
 				viewPort.y = 0 - this.getPositionY();
 				//Update the viewport
-				CCTMXTiledMap.viewPort = viewPort;
+				//CCTMXTiledMap.viewPort = viewPort;
 				//trace(viewPort);
 			} else {
 				isMove = false;
 				offset.x = 0;
 				offset.y = 0;
+				this.player.stopAllActions();
 			}
-			
-			
 		}
-		
 	}
 	
 	public function initCenterPoint(pos : Point) {
-		trace("pos = " + pos);
+		//trace("pos = " + pos);
 		var x : Float = this.getPositionX();
 		var y : Float = this.getPositionY();
 		
@@ -95,7 +103,7 @@ class TestLayer extends CCLayer
 		
 		var diffx = x + width / 2 - pos.x;
 		var diffy = y + height / 2 - pos.y;
-		trace(diffx, diffy);
+		//trace(diffx, diffy);
 		
 		this.setPosition(x + diffx, y + diffy);
 	}
@@ -153,101 +161,130 @@ class TestLayer extends CCLayer
 	
 	var start : Point;
 	var end : Point;
+	
+	var des : Point;
 	override public function onPointerDown(event:CCPointer):Bool 
 	{
+		
 		start = event.getLocation();
-		trace(start);
-		var pos : Point = event.getLocation();
+		//trace(start);
+		//var pos : Point = event.getLocation();
 		
 		
-		if (pos.y <= 100) {
-			offset.y -= speedY;
-			isMove = true;
-		} else if(pos.y > 300){
-			offset.y += speedY;
-				isMove = true;
-		} else if (pos.x < 100) {
-			offset.x -= speedX;
-				isMove = true;
-		} else if (pos.x > 220) {
-			offset.x += speedX;
-				isMove = true;
+		//if (pos.y <= 100) {
+			//offset.y -= speedY;
+			//isMove = true;
+		//} else if(pos.y > 300){
+			//offset.y += speedY;
+				//isMove = true;
+		//} else if (pos.x < 100) {
+			//offset.x -= speedX;
+				//isMove = true;
+		//} else if (pos.x > 220) {
+			//offset.x += speedX;
+				//isMove = true;
+		//}
+		des = new Point(event.getLocation().x + viewPort.x, event.getLocation().y + viewPort.y);
+		//trace(des);
+		
+		
+		var disX = des.x - player.getPositionX();
+		var disY = des.y - player.getPositionY();
+		
+		if (disX == 0 && disY == 0) {
+			return true;
 		}
+		
+		var l : Float = disX / disY;
+		
+		speedX = Math.sqrt((l * l * playerVec * playerVec) / (1 + l * l));
+		speedY = Math.sqrt((playerVec * playerVec) / (1 + l * l));
+		
+		if (disX < 0) {
+			speedX = -speedX;
+		}
+		
+		if (disY < 0) {
+			speedY = -speedY;
+		}
+		isMove = true;
 		
 		return true;
 	}
 	
-	override public function onPointerDragged(event:CCPointer):Bool 
-	{
-		var x = this.getPositionX();
-		var y = this.getPositionY();
-		
-		x = x + event.getLocation().x - start.x;
-		y = y + event.getLocation().y - start.y;
-		this.setPosition(x, y);
-	
-		
-		start = event.getLocation();
-		
-		isMove = false;
-				offset.x = 0;
-				offset.y = 0;
-		return false;
-	}
+	//override public function onPointerDragged(event:CCPointer):Bool 
+	//{
+		//var x = this.getPositionX();
+		//var y = this.getPositionY();
+		//
+		//x = x + event.getLocation().x - start.x;
+		//y = y + event.getLocation().y - start.y;
+		//this.setPosition(x, y);
+	//
+		//
+		//start = event.getLocation();
+		//
+		//isMove = false;
+				//offset.x = 0;
+				//offset.y = 0;
+				//
+		//isMove = false;
+		//return false;
+	//}
 	
 	override public function onPointerUp(event:CCPointer):Bool 
 	{
 		isMove = false;
-				offset.x = 0;
-				offset.y = 0;
+		//offset.x = 0;
+		//offset.y = 0;
+		player.stopAllActions();
 		return true;
 	}
 	
-	var speedY = 32;
-	var speedX = 32;
-	override public function onKeyDown(event:KeyboardEvent)
-	{
-		//var position : Point = player.getPosition();
-		switch(event.key) {
-			case Key.Up :
-				offset.y -= speedY;
-				isMove = true;
-			case Key.Down :
-				offset.y += speedY;
-				isMove = true;
-			case Key.Right :
-				offset.x += speedX;
-				isMove = true;
-			case Key.Left :
-				offset.x -= speedX;
-				isMove = true;
-			default :
-				null;
-				
-		}
-	}
 	
-	override public function onKeyUp(event:KeyboardEvent) {
-		switch(event.key) {
-			case Key.Up :
-				isMove = false;
-				offset.x = 0;
-				offset.y = 0;
-			case Key.Down :
-				isMove = false;
-				offset.x = 0;
-				offset.y = 0;
-			case Key.Right :
-				isMove = false;
-				offset.x = 0;
-				offset.y = 0;
-			case Key.Left :
-				isMove = false;
-				offset.x = 0;
-				offset.y = 0;
-			default :
-				null;
-				
-		}
-	}
+	//override public function onKeyDown(event:KeyboardEvent)
+	//{
+		//var position : Point = player.getPosition();
+		//switch(event.key) {
+			//case Key.Up :
+				//offset.y -= speedY;
+				//isMove = true;
+			//case Key.Down :
+				//offset.y += speedY;
+				//isMove = true;
+			//case Key.Right :
+				//offset.x += speedX;
+				//isMove = true;
+			//case Key.Left :
+				//offset.x -= speedX;
+				//isMove = true;
+			//default :
+				//null;
+				//
+		//}
+	//}
+	//
+	//override public function onKeyUp(event:KeyboardEvent) {
+		//switch(event.key) {
+			//case Key.Up :
+				//isMove = false;
+				//offset.x = 0;
+				//offset.y = 0;
+			//case Key.Down :
+				//isMove = false;
+				//offset.x = 0;
+				//offset.y = 0;
+			//case Key.Right :
+				//isMove = false;
+				//offset.x = 0;
+				//offset.y = 0;
+			//case Key.Left :
+				//isMove = false;
+				//offset.x = 0;
+				//offset.y = 0;
+			//default :
+				//null;
+				//
+		//}
+	//}
 }
